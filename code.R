ba9f4695f22932be55538727d63892e7ba2843ff
Shinya Uryu <suika1127@gmail.com>
2022-12-09 10:42:17 +0900

    [add]packages

--- code.R
+++ code.R
@@ -5,5 +5,12 @@ library(targets)
 library(tarchetypes)
 library(ragg)
 library(here)
+library(ssdse)
+library(patchwork)
+library(tsibble) # 時系列データへの変換
+library(feasts) # 時系列データ操作
+library(jmastats) # 
 library(rnaturalearth)
 library(rnaturalearthhires)
+library(covidregionaldata) # https://github.com/epiforecasts/covidregionaldata
+library(pins)
